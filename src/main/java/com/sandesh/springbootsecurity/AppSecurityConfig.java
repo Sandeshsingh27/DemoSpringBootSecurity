594e02930c37ab59d508fe8193463aa99742722c
Sandesh Singh <sandeshsingh295@gmail.com>
2024-06-18 16:47:05 +0530

    fetched username and password from the database

--- src/main/java/com/sandesh/springbootsecurity/AppSecurityConfig.java
+++ src/main/java/com/sandesh/springbootsecurity/AppSecurityConfig.java
@@ -1,14 +1,22 @@
 package com.sandesh.springbootsecurity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,15 +37,28 @@ public class AppSecurityConfig {
         return http.build();
     }
     
-    @Bean
-    public UserDetailsService userDetailsService() {
-        // Define an in-memory user store with a single user
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("Sandesh")
-            .password("1234")
-            .roles("USER")
-            .build();
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        // Define an in-memory user store with a single user
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//            .username("Sandesh")
+//            .password("1234")
+//            .roles("USER")
+//            .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+    
+    @Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public AuthenticationProvider authProvider()
+	{
+		DaoAuthenticationProvider provider =new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
+	}
 
-        return new InMemoryUserDetailsManager(user);
-    }
 }
